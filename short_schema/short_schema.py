c6b9259526aad899ac578570e95314d1c11ba12d
Tal Wrii <talwrii@gmail.com>
2017-11-06 21:00:52 +0100

    Refactoring in preparation for pretty formatting

--- short_schema/short_schema.py
+++ short_schema/short_schema.py
@@ -3,53 +3,59 @@ import argparse
 import sys
 
 def build_parser():
-    return argparse.ArgumentParser(description='A succinct language for json schema', prog='short_schema')
+    parser = argparse.ArgumentParser(description='A succinct language for json schema', prog='short_schema')
+    parser.add_argument('--pretty', '-p', action='store_true', default=False, help='Spread the output over multiple lines')
+    return parser
 
-def encode(item):
-    if item['type'] == 'array':
-        return encode_array(item)
-    elif item['type'] == 'object':
-        return encode_object(item)
-    else:
-        return encode_type(item['type'])
+class Encoder(object):
 
-def encode_type(_type):
-    if _type == 'string':
-        return 'str'
-    elif _type == 'integer':
-        return 'integer'
-    elif _type == 'number':
-        return 'number'
-    elif _type == 'null':
-        return 'null'
-    elif isinstance(_type, list):
-        return ' | '.join(map(encode_type, _type))
-    else:
-        raise NotImplementedError(_type)
+    def __init__(self, pretty):
+        self.pretty = pretty
 
-def encode_type_arrays(array):
-    return '[{}]'.format(', '.join(map(encode, array)))
+    def encode(self, item):
+        if item['type'] == 'array':
+            return self.encode_array(item)
+        elif item['type'] == 'object':
+            return self.encode_object(item)
+        else:
+            return self.encode_type(item['type'])
 
-def encode_object(item):
-    properties = item.get('properties', {})
-    required = set(item.get('required', []))
-    text = [encode_property(name, schema, name not in required) for name, schema in sorted(properties.items())]
+    def encode_type(self, _type):
+        if _type == 'string':
+            return 'str'
+        elif _type == 'integer':
+            return 'integer'
+        elif _type == 'number':
+            return 'number'
+        elif _type == 'null':
+            return 'null'
+        elif isinstance(_type, list):
+            return ' | '.join(map(self.encode_type, _type))
+        else:
+            raise NotImplementedError(_type)
 
-    return '{}{}{}'.format('{', ', '.join(text), '}')
+    def encode_type_arrays(self, array):
+        return '[{}]'.format(', '.join(map(self.encode, array)))
 
-def encode_array(item):
-    if not item['items']:
-        return '[]'
-    if 'properties' not in item['items']:
-        return '[{}]'.format(encode(item['items']))
-    else:
-        return '[{}]'.format(encode_object(item['items']))
+    def encode_object(self, item):
+        properties = item.get('properties', {})
+        required = set(item.get('required', []))
+        text = [self.encode_property(name, schema, name not in required) for name, schema in sorted(properties.items())]
+        return '{}{}{}'.format('{', ', '.join(text), '}')
 
-def encode_property(name, schema, optional):
-    optional_flag = '?' if optional else ''
-    return optional_flag + '{}: {}'.format(name, encode(schema))
+    def encode_array(self, item):
+        if not item['items']:
+            return '[]'
+        if 'properties' not in item['items']:
+            return '[{}]'.format(self.encode(item['items']))
+        else:
+            return '[{}]'.format(self.encode_object(item['items']))
+
+    def encode_property(self, name, schema, optional):
+        optional_flag = '?' if optional else ''
+        return optional_flag + '{}: {}'.format(name, self.encode(schema))
 
 def main():
-    _args = build_parser().parse_args()
+    args = build_parser().parse_args()
     data = json.loads(sys.stdin.read())
     print encode(data)
